01106c376e8061b0d07de513731ee6c5ba383ec2
jzwar <jacqueszwar@gmail.com>
2024-04-08 23:22:34 +0200

    Added second test case MBB

--- examples/StructuralOptimization/CantileverDesign/plot_parameter_spline.py
+++ examples/StructuralOptimization/CantileverDesign/plot_parameter_spline.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import splinepy as sp
 
@@ -6,15 +7,16 @@ macro_spline = sp.Bezier(
     degrees=[1, 1],
     control_points=[
         [0.0, 0.0],
-        [2.0, 0.0],
-        [0.0, 1.0],
-        [2.0, 1.0],
+        [5.0, 0.0],
+        [0.0, 2.0],
+        [5.0, 2.0],
     ],
 )
 
 # Create parameters spline
 parameter_spline_degrees = [0, 0]
-parameter_spline_cps_dimensions = [24, 12]
+parameter_spline_cps_dimensions = [15, 6]
+tiling = [30, 12]
 parameter_spline = sp.BSpline(
     degrees=parameter_spline_degrees,
     knot_vectors=[
@@ -37,14 +39,19 @@ parameter_spline = sp.BSpline(
 # Read data from file
 directory = (
     "/Users/jzwar/Git/pygadjoints/examples/"
-    "StructuralOptimization/CantileverDesign/"
+    "StructuralOptimization/MBBDesign/deg_1/"
 )
 file_name = "log_file_iterations.csv"
 iterations = np.genfromtxt(directory + file_name, delimiter=",")
 best_value_id = np.argmin(iterations[:, 1])
 parameters = iterations[best_value_id, 2:]
 
-parameter_spline.cps[:] = parameters.reshape(-1, 1)
+
+plt.plot(iterations[:, 1])
+plt.show()
+
+
+parameter_spline.cps[:] = parameters.reshape(-1, 1)/5
 macro_spline.spline_data["parameter_spline"] = parameter_spline
 macro_spline.show_options["data"] = "parameter_spline"
 macro_spline.show_options["control_points"] = False
@@ -52,3 +59,27 @@ macro_spline.show_options["knots"] = False
 macro_spline.show_options["cmap"] = "jet"
 
 macro_spline.show(resolutions=1000)
+
+generator = sp.microstructure.Microstructure(
+    deformation_function=macro_spline,
+    tiling=tiling,
+    microtile=sp.microstructure.tiles.DoubleLattice(),
+)
+
+for i in range(iterations.shape[0]):
+    parameters = iterations[i, 2:]
+    parameter_spline.cps[:] = parameters.reshape(-1, 1)/5
+
+    def parameter_function(x):
+        return np.tile(parameter_spline.evaluate(x), [1, 2])
+
+    generator.parametrization_function = parameter_function
+
+    microstructure = generator.create()
+
+    microstructure.show(
+        control_points=False,
+        knots=False,
+        c="grey",
+        resolutions=2
+    )
